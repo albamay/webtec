4c7221234c4316dbd0a86877d787d1b99653d0cb
albamay <greta.ng99@gmail.com>
2023-09-13 16:24:41 +0200

    Fortsättning av inlämning

--- me/report/public/me.php
+++ me/report/public/me.php
@@ -22,7 +22,7 @@ include('../view/header.php');
             <figcaption>Paddan</figcaption>
         </figure>
 
-        <p2>Hej! Jag är paddan och är duktig på att skapa problem och spendera timmar på att lösa dom. Så programering är perfekt för mig....gillar jag att tro. Älskar regn, lugna dagar och vackra saker. Vackra saker som skog, målningar, berg och mjuka kuddar. Jag är här för att lära mig att programera och få jobb inom området. Jag älskar min damm så mycket att jag har valt att studera distans i webbprogrammering för 120 poäng. Har studerat tidigare inom filisofi, politik och ekonomi, men vill skriva min kandidat inom programmering</p2> 
+        <p>Hej! Jag är paddan och är duktig på att skapa problem och spendera timmar på att lösa dom. Så programering är perfekt för mig....gillar jag att tro. Älskar regn, lugna dagar och vackra saker. Vackra saker som skog, målningar, berg och mjuka kuddar. Jag är här för att lära mig att programera och få jobb inom området. Jag älskar min damm så mycket att jag har valt att studera distans i webbprogrammering för 120 poäng. Har studerat tidigare inom filisofi, politik och ekonomi, men vill skriva min kandidat inom programmering</p> 
     
 
         <footer class="byline">
